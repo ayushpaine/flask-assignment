a2a1145d2b5d72f571c692c7fa6d93b49edb7167
ayushpaine <ayushmt701@gmail.com>
2022-08-04 19:03:07 +0530

    rendered list of 50 users based on url parameter

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import collections
+from crypt import methods
 import json
 from flask import Flask, render_template
 from flask import jsonify
 from collections import OrderedDict
+from matplotlib import use
+from requests import request
 
 from sklearn.exceptions import DataDimensionalityWarning
 
 app = Flask(__name__)
 
 
-@app.route('/<int:number>')
+@app.route('/<int:number>', methods=['GET'])
 def data(number):
     datadict = OrderedDict()
     for i in range(number, number + 50):
@@ -17,7 +20,24 @@ def data(number):
                                 'phone': f"phone{i}", "email": f"email{i}", "timestamp": "1st aug 9am"}
 
     data = json.dumps(datadict, indent=3)
-    return render_template("index.html", data=data)
+    response = app.response_class(
+        response=data,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/users/<int:user_id>', methods=["GET"])
+def renderData(user_id):
+
+    return render_template("index.html", user_id=user_id)
+
+
+@app.route("/", methods=["GET"])
+def home():
+
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
